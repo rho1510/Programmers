24fa55b1666a72881868e3c1b02c1999cc1c7855
rho1510 <56241084+rho1510@users.noreply.github.com>
2023-10-30 22:47:41 +0900

    [level unrated] Title: 문자열 섞기, Time: 14.89 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -4,12 +4,8 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         
-        char[] a = str1.toCharArray();
-        char[] b = str2.toCharArray();
-        
         for(int i=0; i<str1.length();i++){
-            answer+=a[i];
-            answer+=b[i];
+            answer+=""+str1.charAt(i)+str2.charAt(i);
         }
         
         return answer;
