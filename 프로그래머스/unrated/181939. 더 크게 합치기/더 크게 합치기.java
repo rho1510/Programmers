1c745c7916fefc00d88463ec87fe408456bc7a2e
rho1510 <56241084+rho1510@users.noreply.github.com>
2023-10-31 11:21:46 +0900

    [level unrated] Title: 더 크게 합치기, Time: 1.09 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String sum1 = Integer.toString(a)+Integer.toString(b);
+        String sum2 = Integer.toString(b)+Integer.toString(a);
+        
+         if(Integer.parseInt(sum1)>Integer.parseInt(sum2)){
+            answer=Integer.parseInt(sum1);
+        }
+        else{
+            answer=Integer.parseInt(sum2);
+        }
+        
+        return answer;
+    }
+}
