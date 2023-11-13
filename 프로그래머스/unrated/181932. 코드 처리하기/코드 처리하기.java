e5a0990e513ca0ab5190bf99eadd3d91d5c2b7be
rho1510 <56241084+rho1510@users.noreply.github.com>
2023-11-13 21:29:24 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        
+        char[] arr = code.toCharArray();
+        
+        for(int i = 0; i < arr.length; i++){
+           if(mode==0){
+               if(arr[i]!='1'){
+                   if(i%2==0){
+                       answer+=arr[i];
+                   }
+               }
+               else{
+                   mode=1;
+               }
+           }
+            else{
+                if(arr[i]!='1'){
+                    if(i%2==1){
+                        answer+=arr[i];
+                    }
+                }
+                else{
+                    mode=0;
+                }
+            }
+        }
+        
+        if(answer==""){
+            return "EMPTY";
+        }
+        
+        
+        return answer;
+    }
+}
