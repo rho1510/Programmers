30506f646ee130019030f7db89fcac5ff39551e1
rho1510 <56241084+rho1510@users.noreply.github.com>
2023-11-14 23:33:38 +0900

    [level 1] Title: 공원 산책, Time: 0.20 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/README.md
+++ 프로그래머스/1/172928. 공원 산책/README.md
@@ -1,10 +1,10 @@
 # [level 1] 공원 산책 - 172928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172928#) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 0.19 ms
+메모리: 74.6 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 2일 23:26:12
+2023년 11월 2일 23:33:37
 
 ### 문제 설명
 
--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -5,6 +5,7 @@ class Solution {
         int[] answer = {};
         int[] start = new int[2];
         
+        // 시작 지점 찾기
         for(int i=0;i<park.length;i++){
            if(park[i].contains("S")){
                start[0] = i;
@@ -13,23 +14,31 @@ class Solution {
            }
         }
         
+        // routes 수행 
         for(int j=0;j<routes.length;j++){
+            // routes 방향
             char direction = routes[j].charAt(0);
+            // routes 거리
             int distance = Character.getNumericValue(routes[j].charAt(2));
-            
+            // 장애물 판별 
             boolean flag = false;
             
+            // 서쪽이니까 - 라서 0 이상이면 됨
             if(direction == 'W' && start[1] - distance >= 0){
+                // 장애물 판별 // park에서 시작점 - 거리 중에 X가 있는지 
                 for(int k=1;k<=distance;k++){
                     if(park[start[0]].charAt(start[1]-k)=='X'){
+                        // 장애물 있다면 반복문 break
                         flag = true;
                         break;
                     }
                 }
+                // 장애물이 없다면 거리 이동
                 if(!flag){
                     start[1] -= distance;
                 }
             }
+            // 동쪽이니까 + 라서 park 배열 안의 String 길이보다만 작으면 됨
             else if (direction == 'E' && start[1] + distance < park[0].length()) {
                 for(int k=1;k<=distance;k++){
                     if(park[start[0]].charAt(start[1]+k)=='X'){
@@ -41,6 +50,7 @@ class Solution {
                     start[1] += distance;
                 }
             } 
+            // 북 -> 0 이상이면 됨
             else if (direction == 'N' && start[0] - distance >= 0) {
                 for(int k=1;k<=distance;k++){
                     if(park[start[0]-k].charAt(start[1])=='X'){
@@ -52,6 +62,7 @@ class Solution {
                     start[0] -= distance;
                 }
             } 
+            // 남 -> park 배열의 길이보다 작으면 됨
             else if (direction == 'S' && start[0] + distance < park.length) {
                 for(int k=1;k<=distance;k++){
                     if(park[start[0]+k].charAt(start[1])=='X'){
@@ -64,7 +75,6 @@ class Solution {
                 }
             }
         }
-        
         
         return start;
     }
